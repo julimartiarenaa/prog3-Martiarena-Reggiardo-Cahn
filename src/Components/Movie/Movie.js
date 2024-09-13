419cebedc375d5d92634227f7250fc010f120c49
abrilreggii <areggiardo@udesa.edu.ar>
2024-09-13 13:03:16 -0300

    cambios

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -1,16 +1,19 @@
 import React, {Component} from "react";
-import "../MovieElement/MovieElement"
+import MovieElement from "../MovieElement/MovieElement";
+
+let APIKey = "7bbfa155b881d065cf760bebe36c4d28"
 
 class Movie extends Component{
     constructor(){
         super();
         this.state={
             movies: [],
+
         }
 
     }
     componentDidMount(){
-        fetch('https://api.themoviedb.org/3/7bbfa155b881d065cf760bebe36c4d28')
+        fetch(`https://api.themoviedb.org/3/movie/?api_key=${APIKey}`)
             .then(response => response.json())
             .then(data => {
                 console.log(data)
@@ -23,7 +26,7 @@ class Movie extends Component{
     render(){
         return(
             <React.Fragment> 
-                {this.state.movies === 0 ? <h3> Cargando...</h3> : <MovieElement movies = {this.state.movies}/> }
+                {this.state.movies === 0 ? <h3> Cargando...</h3> : <MovieElement Movies = {this.state.movies}/> }
             </React.Fragment>
         )
     }
