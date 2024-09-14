f08150ed99ce16b1345f6996804220acd05d0256
abrilreggii <areggiardo@udesa.edu.ar>
2024-09-14 10:55:35 -0300

    funciona el home, sin el css

--- src/Components/MovieElement/MovieElement.js
+++ src/Components/MovieElement/MovieElement.js
@@ -1,17 +1,25 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class Element extends Component {
-    constructor(props){
+    constructor(props) {
         super()
     }
-    render(){
-        return(
+    render() {
+
+        if (!this.props.data.poster_path) {
+            return <div>Cargando...</div>;
+        }
+
+        return (
             <div className="element">
-                {this.props.data}
+                <img src={`https://image.tmdb.org/t/p/w500${this.props.data.poster_path}`} alt="Poster" />
+                <h1>{this.props.data.title}</h1>
+                <button>Ver descripción</button>
+                <button>Ir a detalle</button>
+                <button>Agregar a favoritos</button>
             </div>
-        )
+        );
     }
-    
 }
 
 export default Element;
