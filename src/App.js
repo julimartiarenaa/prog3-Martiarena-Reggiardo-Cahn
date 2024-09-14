d23a36ac94e96e177add20ad0722a84a4716e414
julimartiarenaa <jmartiarena@udesa.edu.ar>
2024-09-14 11:13:12 -0300

    por las dudas pq toque el app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from "react";
 import './css/styles.css'
 import { Switch, Route } from "react-router-dom/cjs/react-router-dom";
-import Header from "./Components/Header/Header";
 import Home from "./screens/Home/Home";
 import NotFound from "./screens/NotFound/NotFound";
 
 
 function App() {
   return (
-    <React.Fragment>
-      <Header />
-      <Switch>
-        <Route path="/" component={Home}></Route>
-        <Route path="" component={NotFound}></Route>
-      </Switch>
-    </React.Fragment>
+    <Switch>
+      <Route path="/" exact ={true} component={Home}></Route>
+      <Route path="" component={NotFound}></Route>
+    </Switch>
   );
 }
 
